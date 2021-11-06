fd9263b2c55190a905006dcc26975dcc1a315273
mk1020 <mmmffjs7438g@gmail.com>
2021-11-06 22:46:22 +0200

    delete payments

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ import {AppRegistry} from 'react-native';
 import {AppContainer} from './src/AppContainer';
 import {name as appName} from './app.json';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-global.PaymentRequest = require('react-native-payments').PaymentRequest;
 AppRegistry.registerComponent(appName, () => AppContainer);
